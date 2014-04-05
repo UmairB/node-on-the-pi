f416dfc34eb2da84bc74c6a9f3fc5859b23bc11a
root <root@ubuntu-mac.(none)>
2014-04-05 22:55:57 +0100

    Ran nyp to build the c++ code

---
+++ cpp-integration/build/Release/.deps/Release/obj.target/hello/hello.o.d
@@ -0,0 +1,21 @@
+cmd_Release/obj.target/hello/hello.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/umair/.node-gyp/0.10.26/src -I/home/umair/.node-gyp/0.10.26/deps/uv/include -I/home/umair/.node-gyp/0.10.26/deps/v8/include  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer -fno-rtti -fno-exceptions -MMD -MF ./Release/.deps/Release/obj.target/hello/hello.o.d.raw  -c -o Release/obj.target/hello/hello.o ../hello.cc
+Release/obj.target/hello/hello.o: ../hello.cc \
+ /home/umair/.node-gyp/0.10.26/src/node.h \
+ /home/umair/.node-gyp/0.10.26/deps/uv/include/uv.h \
+ /home/umair/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-unix.h \
+ /home/umair/.node-gyp/0.10.26/deps/uv/include/uv-private/ngx-queue.h \
+ /home/umair/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-linux.h \
+ /home/umair/.node-gyp/0.10.26/deps/v8/include/v8.h \
+ /home/umair/.node-gyp/0.10.26/deps/v8/include/v8stdint.h \
+ /home/umair/.node-gyp/0.10.26/src/node_object_wrap.h \
+ /home/umair/.node-gyp/0.10.26/src/node.h
+../hello.cc:
+/home/umair/.node-gyp/0.10.26/src/node.h:
+/home/umair/.node-gyp/0.10.26/deps/uv/include/uv.h:
+/home/umair/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-unix.h:
+/home/umair/.node-gyp/0.10.26/deps/uv/include/uv-private/ngx-queue.h:
+/home/umair/.node-gyp/0.10.26/deps/uv/include/uv-private/uv-linux.h:
+/home/umair/.node-gyp/0.10.26/deps/v8/include/v8.h:
+/home/umair/.node-gyp/0.10.26/deps/v8/include/v8stdint.h:
+/home/umair/.node-gyp/0.10.26/src/node_object_wrap.h:
+/home/umair/.node-gyp/0.10.26/src/node.h:
